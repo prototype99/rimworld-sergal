8a6ca8d382607531235dfe59291d9cdd4e4ccd69
Seirra <sophietheopossum@yandex.ru>
2025-10-15 03:47:04 +0100

    Clean up unused `using` directives
    Removes several unnecessary namespace imports from the ability definition.
    Improves code clarity and reduces clutter by ensuring only required namespaces are referenced.

--- Source/AufenCode/AufenBetty_Ability.cs
+++ Source/AufenCode/AufenBetty_Ability.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RimWorld;
+﻿using RimWorld;
 using Verse;
-using UnityEngine;
 using Verse.Sound;
 
 namespace AufenCode
